fd830c86c77dff98d75ca6d037edd217f8475781
blink1073 <steven.silvester@ieee.org>
2014-06-28 12:36:48 -0500

    Prep for gh-pages

--- README.rst
+++
@@ -1,4 +0,0 @@
-auto-enaml
-============
-
-Auto-Enaml provides utilities for the auto generation of Enaml widgets based on Atom classes.
--- auto_enaml/__init__.py
+++
@@ -1,7 +0,0 @@
-
-import enaml
-
-with enaml.imports():
-    from .auto_editors import AutoView, AutoItem, AutoWindow
-
-from .auto_view import auto_view, auto_item, auto_window
