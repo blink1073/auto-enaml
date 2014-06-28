e736f5a57bcb9b6e0f6783196b6215ad008bca17
blink1073 <steven.silvester@ieee.org>
2014-06-28 13:03:25 -0500

    Update docs

--- auto_enaml/auto_view.py
+++ auto_enaml/auto_view.py
@@ -7,6 +7,23 @@ with enaml.imports():
 
 
 def auto_item(info,  **kwargs):
+    """Generate a Label-Editor combo tied to a particular `Atom` member.
+
+    Parameters
+    ----------
+    info : tuple of (atom_instance, string) or a string
+        If used within an auto_view, a string can be used for the member
+        name.  Otherwise, a tuple with the atom_instance and the name
+        must be used.
+    kwargs : dictionary
+        Keyword arguments to pass to the widgets.  Label-specific
+        properties can be set by using a "label_" prefix
+        (e.g. label_tooltip).  The same is true for the editor.
+
+    Returns
+    -------
+    item : AutoItem enaml instance
+    """
     if isinstance(info, str):
         return Partial(info, **kwargs)
     elif isinstance(info, tuple) and len(info) == 2:
@@ -30,6 +47,19 @@ def auto_item(info,  **kwargs):
 
 def auto_view(model, *objects, **kwargs):
     """ Generate a view directly from an `Atom` instance.
+
+    Parameters
+    ----------
+    model : Instance of Atom-based class.
+    objects : str or auto_items
+        Arguments used to build the auto_item editors.  Can be a name
+        of a member (string), or an auto_item.
+    kwargs : dictionary
+        Keyword arguments to pass to the view.
+
+    Returns
+    -------
+    item : AutoView enaml instance
     """
     objects = list(objects)
     for (ind, obj) in enumerate(objects):
@@ -42,6 +72,19 @@ def auto_view(model, *objects, **kwargs):
 
 def auto_window(model, *objects, **kwargs):
     """ Generate a window directly from an `Atom` instance.
+
+    Parameters
+    ----------
+    model : Instance of Atom-based class or an auto_view
+    objects : str or auto_items
+        Arguments used to build the auto_item editors.  Can be a name
+        of a member (string), or an auto_item.
+    kwargs : dictionary
+        Keyword arguments to pass to the window.
+
+    Returns
+    -------
+    item : AutoWindow enaml instance
     """
     if isinstance(model, PyAutoView):
         return PyAutoWindow(view=model, **kwargs)
